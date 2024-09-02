dddfe98a4956e90a80935a463cc631e3d003e05e
Agaano <klapoplapo@gmail.com>
2024-09-02 18:33:04 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,240 +1,143 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import { useModal} from './useModal';
+import useGameLogic from './useGameLogic'
+export type Player = {
+  x: number,
+  y: number,
+  name: string,
+  speed: number,
+  shootCooldown: number,
+  shootTime: number,
+  bulletSpeed: number,
+  hits: number,
+  color: string
+}
+
+export type Bullet = {
+  x: number,
+  y: number,
+  speed: number,
+  hit: boolean,
+  color: string,
+}
+
+export type GameData = {
+  gameboardSize: {width: number, height: number},
+  players: Player[],
+  bullets: Map<number, Bullet>,
+  playerRadius: number,
+  racketWidth: number,
+}
 
 function App() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const Modal1 = useModal()
-  const [Modal1IsOpen, setModal1IsOpen] = useState(false);
-  const Modal2 = useModal()
-  const [Modal2IsOpen, setModal2IsOpen] = useState(false);
-  
+  const gameboardSize = {width: 750, height: 500}
+  const playerRadius = 10
 
-  function drawLine(ctx: CanvasRenderingContext2D, from: {x: number, y: number}, to: {x: number, y: number}, color?: string) {
-    if (!!color) ctx.strokeStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(from.x, from.y);
-    ctx.lineTo(to.x, to.y);
-    ctx.stroke();
-    ctx.closePath();
-  }
-
-  type Player = {
-    position: {x: number, y: number},
-    name: string,
-    speed: number,
-    shootCooldown: number,
-    shootTime: number,
-    bulletSpeed: number,
-    hits: number,
-    color: string,
-  }
-
-  type Bullet = {
-    position: {x: number, y: number},
-    speed: number,
-    used: boolean,
-    color: string,
-  }
-  
-  function Start(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
-    const playableBoardSize = {width: canvas.width - 20, height: canvas.height - 30}
-    const canvasSize = {width: canvas.width, height: canvas.height}
-    const radius = 10;
-    const racketWidth = 30;
-    const players: Player[] = [{
-      position: {x: 20, y: 30},
-      name: "Player1",
+  const [gameData, setGameData] = useState<GameData>({
+    players: [{
+      x: 20,
+      y: 10 + playerRadius,
+      name: "Player1", 
       speed: 5,
-      shootCooldown: 10,
+      shootCooldown: 0.1,
       shootTime: 0,
       bulletSpeed: 5,
       hits: 0,
-      color: "#0f0"
-    },
-    {
-      position: {x: canvasSize.width - 20, y: canvasSize.height - 31},
-      name: "Player2",
+      color: "#ff0000",
+    },{
+      x: gameboardSize.width - 20,
+      y: gameboardSize.height - (10 + playerRadius),
+      name: "Player2", 
       speed: -5,
-      shootCooldown: 10,
+      shootCooldown: 5,
       shootTime: 0,
       bulletSpeed: -5,
       hits: 0,
-      color: "#f00"
-    }]
-    const mousePos = {x: 0, y: 0}
-    const bullets = new Map<number, Bullet>()
-    
+      color: "#00ff00",
+    }],
+    bullets: new Map(),
+    gameboardSize,
+    playerRadius,
+    racketWidth: 30
+  });
 
-    function Step(player: Player): Player { 
-      const nextPos = player.position.y + player.speed
-      if ((nextPos <= 30 || nextPos  >= playableBoardSize.height)) player.speed *= -1;
-      if ((nextPos + radius >= mousePos.y && nextPos - radius <= mousePos.y) &&
-          (mousePos.x >= player.position.x - (radius + racketWidth / 2) && mousePos.x <= player.position.x + (radius + racketWidth / 2))  
-      ) player.speed *= -1;
-      player.position.y += player.speed;
-      bullets.forEach((bullet, key, map) => {
-        if (bullet.speed !== player.bulletSpeed) {
-          if ((bullet.position.x >= player.position.x - radius && bullet.position.x <= player.position.x + radius) &&
-              (bullet.position.y >= player.position.y - radius && bullet.position.y <= player.position.y + radius) && !bullet.used
-          ) {
-            bullet.used = true;
-            map.delete(key)
-            player.hits++;
-          }
-        } 
-      })
-      return player;
-    }
+  const ref = useRef<HTMLCanvasElement | null>(null);
+  const GameStart = useGameLogic({gameData, setGameData});
 
-    function Shoot(player: Player) {
-      const bullet = {
-        position: {x: player.position.x, y: player.position.y},
-        speed: player.bulletSpeed,
-        used: false,
-        color: "#fff",
-      };
-      bullets.set(new Date().getTime() + player.bulletSpeed, bullet)
-      console.log(bullets)
-    }
-
-    function DrawCircle(position: {x: number, y: number}, radius: number, color: string) {
-      ctx.fillStyle = color;
-      ctx.beginPath()
-      ctx.ellipse(position.x,position.y,radius,radius,0,0,Math.PI * 2);
-      ctx.fill();
-    }
-
-    function StepBullet(bullet: Bullet, bulletKey: number) {
-      const nextPos = bullet.position.x + bullet.speed;
-      if ((nextPos >= canvasSize.width || nextPos <= 0)) {
-        bullets.delete(bulletKey);
-      }
-      bullet.position.x += bullet.speed;
-    }
-    
-    function DrawBoard() {
-      drawLine(ctx, {x: 0, y: 20}, {x: canvasSize.width, y: 20}, "#fff")
-      drawLine(ctx, {x: 0, y: canvasSize.height - 20}, {x: canvasSize.width, y: canvasSize.height - 20}, "#fff")
-    }
-
-    let prevTime = 0;
-
-    function Update(time: number) {
-      const deltaTime = (time - prevTime) / 1000
-
-      ctx.clearRect(0,0,canvasSize.width, canvasSize.height)
-      
-      players.map((player) => {
-        DrawCircle(player.position, radius, player.name === "Player1" ? window.localStorage.getItem("color1") ?? "#f00" : window.localStorage.getItem("color2") ?? "#0f0")
-        player.shootTime += deltaTime
-        if (player.shootTime >= player.shootCooldown) {
-          Shoot(player);
-          player.shootTime = 0;
-        }
-        return Step(player)
-      })
-      
-      DrawBoard();
-
-      ctx.fillStyle = "#fff"
-      ctx.fillRect(mousePos.x - racketWidth / 2, mousePos.y, racketWidth, 5)
-      
-      bullets.forEach((bullet, key) => {
-        DrawCircle(bullet.position, 5, bullet.color)
-        StepBullet(bullet, key);
-      })
-      
-      ctx.fillStyle = "#fff"
-      ctx.fillText(players[0].hits.toString(), 20, 20, 300)
-      ctx.fillText(players[1].hits.toString(), canvasSize.width - 20, 20, 300)
-      
-      prevTime = time;
-      
-      window.requestAnimationFrame(Update);
-    }
-    
-    canvas.onmousemove = (ev) => {
-      mousePos.x = ev.offsetX;
-      mousePos.y = ev.offsetY;
-    }
-
-    canvas.onmouseleave = () => {
-      mousePos.x = -racketWidth;
-      mousePos.y = -racketWidth;
-    }
-
-    const inputs = document.querySelectorAll('input[type="range"]');
-    inputs.forEach((input: Element) => {
-      (input as HTMLInputElement).addEventListener("change", (e) => {
-        const [player, field] = input.id.split(":");
-        if (player === "player1") {
-          //@ts-ignore
-          players[0][field] = +e.target.value;
-        }
-        if (player === "player2") {
-          //@ts-ignore
-          players[1][field] = +e.target.value;
-        }
-      })
-    })
-    window.requestAnimationFrame(Update);
-  }
-
-  function ConfigureAndStart(canvas: HTMLCanvasElement) {
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-    if (!window.localStorage.getItem("color1")) window.localStorage.setItem("color1", "#f00")
-    if (!window.localStorage.getItem("color2")) window.localStorage.setItem("color2", "#0f0")
-    Start(ctx, canvas)
+  function Start() {
+    if (!ref.current) return;
+    const canvas = ref.current;
+    const ctx = ref.current.getContext("2d");
+    if (!ctx) throw Error("Не 2D контекст!")
+    GameStart(ctx, canvas)
   }
 
   useEffect(() => {
-    if (!!canvasRef.current) {
-      ConfigureAndStart(canvasRef.current)
-    }
-  }, [canvasRef.current])
+    Start()
+  }, [ref.current])
+
+  useEffect(() => {
+
+  }, [gameData.players[0]])
 
   return (
-    <div style = {{display: "flex"}}>
-      <div>
-        <div className='range-end'>
-          <label>Bullet Cooldown</label>
-          <input step={0.1} min = {1} max = {5} defaultValue = {3} type='range' id="player1:shootCooldown"/>
-        </div>
-        <div className='range-end'>
-          <label>Speed</label>
-          <input step={0.1} min = {1} max = {10}  type='range' id="player1:speed"/>
-        </div>
-        <div>
-          <button onClick={() => {
-            setModal2IsOpen(true)
-          }}>Change color</button>
-        </div>
+    <>
+      <div className='player leftPlayer'>
+        <h3>Player1</h3>
+        <span>Got Hit:</span>
+        <span>{gameData.players[0].hits}</span>
+        <br/>
+        <span>Speed:</span>
+        <input type = "range" min = {2} max = {10} step = {0.5} value = {Math.abs(gameData.players[0].speed)} onChange={(e) => {
+            setGameData(prev => {
+              prev.players[0].speed = +e.target.value;
+              return {...prev};
+            })
+          }}/>
+        <br/>
+        <span>ShootRate:</span>
+        <input type = "range" min = {0.5} max = {5} step = {0.5} value = {gameData.players[0].shootCooldown} onChange={(e) => {
+            setGameData(prev => {
+              prev.players[0].shootCooldown = +e.target.value;
+              return {...prev};
+            })
+          }}/>
+        <input type = "color" value = {gameData.players[0].color} onChange={(e) => {
+            setGameData(prev => {
+              prev.players[0].color = e.target.value;
+              return {...prev}
+            })
+          }}/>
       </div>
-      <canvas ref={canvasRef} width = {750} height={500}/>
-      <div style = {{justifyContent: 'flex-start'}}>
-        <div className='range-start' >
-          <input step={0.1} min = {1} max = {5} defaultValue = {3} type='range' id="player2:shootCooldown"/>
-          <label>Bullet Cooldown</label>
-        </div>
-        <div className='range-start'>
-          <input step={0.1} min = {1} max = {10}  type='range' id="player2:speed"/>
-          <label>Speed</label>
-        </div>
-        <div>
-          <button onClick={() => {
-            setModal1IsOpen(true)
-          }}>Change color</button>
-        </div>
-        <Modal1 open = {Modal1IsOpen} setOpen={setModal1IsOpen}>
-          <input type = "color" value = {window.localStorage.getItem("color2") ?? "#f00"} onChange={(e) => {window.localStorage.setItem("color2", e.target.value)}}/>
-        </Modal1>
-        <Modal2 open = {Modal2IsOpen} setOpen={setModal2IsOpen}>
-          <input type = "color" value = {window.localStorage.getItem("color1") ?? "#0f0"} onChange={(e) => {window.localStorage.setItem("color1", e.target.value)}}/>
-        </Modal2>
+      <canvas ref={ref} width={gameData.gameboardSize.width} height={gameData.gameboardSize.height}/>
+      <div className='player rightPlayer'>
+        <h3>Player2</h3>
+        <span>Got Hit:</span>
+        <span>{gameData.players[1].hits}</span>
+        <br/>
+        <span>Speed:</span>
+        <input type = "range" min = {2} max = {10} step = {0.5} value = {Math.abs(gameData.players[1].speed)} onChange={(e) => {
+            setGameData(prev => {
+              prev.players[1].speed = +e.target.value;
+              return {...prev};
+            })
+          }}/>
+        <br/>
+        <span>ShootRate:</span>
+        <input type = "range" min = {0.5} max = {5} step = {0.5} value = {gameData.players[1].shootCooldown} onChange={(e) => {
+            setGameData(prev => {
+              prev.players[1].shootCooldown = +e.target.value;
+              return {...prev};
+            })
+          }}/>
+        <input type = "color" value = {gameData.players[1].color} onChange={(e) => {
+            setGameData(prev => {
+              prev.players[1].color = e.target.value;
+              return {...prev}
+            })
+          }}/>
       </div>
-    </div>
+    </>
   )
 }
 
---
+++ src/useGameLogic.tsx
@@ -0,0 +1,139 @@
+import { useEffect, useRef, useState } from "react"
+import { Bullet, GameData, Player } from "./App";
+
+export default (props: {gameData: GameData, setGameData: React.Dispatch<React.SetStateAction<GameData>>}) => {
+    const propsRef = useRef(props);
+    const {gameData, setGameData} = propsRef.current;
+    const [ctx, setCTX] = useState<CanvasRenderingContext2D | null>(null);
+    const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
+    const mousePos = {x: 0, y: 0};
+    let prevTime = 0;
+
+    function DrawCircle(x: number, y: number, radius: number, color?: string) {
+        if (!ctx || !canvas) return;
+        if (!!color) ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.ellipse(x,y, radius, radius, 0, 0, Math.PI * 2)
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function DrawLine(fromX: number, fromY: number, toX: number, toY: number, color?: string) {
+        if (!ctx || !canvas) return;
+        if (!!color) ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(fromX,fromY);
+        ctx.lineTo(toX, toY);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    function DrawGrid() {
+        if (!ctx || !canvas) return;
+        DrawLine(0, 10, canvas.width, 10, "white")
+        DrawLine(0, canvas.height - 10, canvas.width, canvas.height - 10, "white")
+    }
+
+    function Shoot(player: Player) {
+        gameData.bullets.set(new Date().getTime() + player.bulletSpeed, {
+            x: player.x,
+            y: player.y,
+            speed: player.bulletSpeed,
+            color: player.color,
+            hit: false
+        })
+    }
+
+    function StepBullet(bullet: Bullet, bulletKey: number) {
+        const nextPos = bullet.x + bullet.speed;
+        if ((nextPos >= gameData.gameboardSize.width || nextPos <= 0)) {
+            gameData.bullets.delete(bulletKey);
+        }
+        bullet.x += bullet.speed;
+        return bullet;
+    }
+
+    function StepPlayer(player: Player) {
+        const nextY = player.y + player.speed;
+
+        const gonnaHitWall = nextY >= gameData.gameboardSize.height - (10 + gameData.playerRadius) || nextY <= 10 + gameData.playerRadius;
+        const gonnaHitRacketY = (nextY + gameData.playerRadius >= mousePos.y && nextY - gameData.playerRadius <= mousePos.y)
+        const gonnaHitRacketX = (mousePos.x >= player.x - (gameData.playerRadius + gameData.racketWidth / 2) && mousePos.x <= player.x + (gameData.playerRadius + gameData.racketWidth / 2))
+        
+        if (gonnaHitWall || (gonnaHitRacketX && gonnaHitRacketY)) 
+            player.speed *= -1;
+
+        gameData.bullets.forEach((bullet, key, map) => {
+            if (bullet.speed !== player.bulletSpeed) {
+              if ((bullet.x >= player.x - gameData.playerRadius && bullet.x <= player.x + gameData.playerRadius) &&
+                  (bullet.y >= player.y - gameData.playerRadius && bullet.y <= player.y + gameData.playerRadius) && !bullet.hit
+              ) {
+                bullet.hit = true;
+                player.hits++;
+                gameData.bullets.delete(key)
+              }
+            } 
+        })
+        
+        player.y += player.speed;
+        return player;
+    }
+
+    function Render(time: number) {
+        if (!ctx || !canvas) return;
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        const deltaTime = (time - prevTime) / 1000;
+        
+        DrawGrid()
+        
+        ctx.fillStyle = "#fff"
+        ctx.fillText(gameData.players[0].name ?? "", 50, 50, 300)
+        const players = [...gameData.players]
+        players.map((player) => {
+            DrawCircle(player.x, player.y, gameData.playerRadius, player.color)
+
+            if (player.shootTime >= player.shootCooldown) {
+                Shoot(player);
+                player.shootTime = 0;
+            } else {
+                player.shootTime += deltaTime;
+            }
+
+            StepPlayer(player);
+        })
+
+        // setGameData(prev => {
+        //     return {...prev, players: [...players]};
+        // })
+
+        gameData.bullets.forEach((bullet, bulletKey) => {
+            DrawCircle(bullet.x, bullet.y, 5, bullet.color)
+            StepBullet(bullet, bulletKey);
+        })
+
+        ctx.fillStyle = "#fff"
+        ctx.fillRect(mousePos.x - gameData.racketWidth / 2, mousePos.y, gameData.racketWidth, 3)
+        setGameData(prev => ({...prev}))
+        prevTime = time;
+        window.requestAnimationFrame(Render);
+    }
+
+    useEffect(() => {
+        propsRef.current = props
+    })
+
+    return (ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) => {
+        setCTX(ctx);
+        setCanvas(canvas);
+        Shoot(gameData.players[0])
+        canvas.addEventListener("mousemove", (e) => {
+            mousePos.x = e.offsetX;
+            mousePos.y = e.offsetY;
+        })
+        canvas.addEventListener("mouseleave", () => {
+            mousePos.x = -gameData.racketWidth;
+            mousePos.y = -gameData.racketWidth;
+        })
+        window.requestAnimationFrame(Render)
+    }
+}
